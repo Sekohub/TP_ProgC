65dc53038fe0c47a9a866b429e3e927d7e17a4ef
SerkanHub <ozerserkan05@gmail.com>
2026-01-29 11:46:18 +0100

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(void)
 {
-    int compteur = 5;  /* doit être strictement inférieur à 10 */
+    int compteur = 5;  
     int i, j;
+
+    printf("=== Version avec boucles for ===\n");
 
     for (i = 1; i <= compteur; i++)
     {
@@ -26,11 +27,35 @@ int main(void)
                 /* intérieur */
                 printf("# ");
             }
-         }
-    }
+        }
         printf("\n");
+    }
 
-return 0;
+    printf("\n=== Version avec boucle while ===\n");
+
+    i = 1;
+    while (i <= compteur)
+    {
+        j = 1;
+        while (j <= i)
+        {
+            if (i == compteur)
+            {
+                printf("* ");
+            }
+            else if (j == 1 || j == i)
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("# ");
+            }
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+
+    return 0;
 }
-    
-
