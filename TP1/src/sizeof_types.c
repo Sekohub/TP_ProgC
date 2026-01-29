52ee427ca97bba3171ea57e42487a75f90f82894
SerkanHub <ozerserkan05@gmail.com>
2026-01-29 10:30:32 +0100

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -4,6 +4,7 @@
 int main ()
 {
   printf("sizeof(char) == %d bits\n", (int) sizeof(char)); //1
+  printf("sizeof(signed char) == %d bits\n", (int) sizeof(signed char); // bonus
   printf("sizeof(short) == %d bits\n", (int) sizeof(short)); //2
   printf("sizeof(int) == %d bits\n", (int) sizeof(int)); //3
   printf("sizeof(long int) == %d bits\n", (int) sizeof(long int)); //4
@@ -14,4 +15,5 @@ int main ()
 
   return 0;
 }
+
 
