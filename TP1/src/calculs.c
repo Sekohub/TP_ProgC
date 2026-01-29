9dea8e595f51b652ec1f93ff801c1ef7db6d93aa
SerkanHub <ozerserkan05@gmail.com>
2026-01-29 12:01:57 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -3,19 +3,20 @@
 
 int main(void)
 {
-  int num1, num2;
-  char op;
+    int num1, num2;
+    char op;
 
-  printf("Entrez le premier nombre entier : ");
+    /* Saisie des valeurs */
+    printf("Entrez le premier nombre entier : ");
     scanf("%d", &num1);
 
     printf("Entrez le second nombre entier : ");
     scanf("%d", &num2);
 
     printf("Entrez l'operateur (+, -, *, /, %%, &, |, ~) : ");
-    scanf(" %c", &op); 
+    scanf(" %c", &op);  /* espace avant %c pour ignorer les retours ligne */
 
-switch (op)
+    switch (op)
     {
         case '+':
             printf("%d + %d = %d\n", num1, num2, num1 + num2);
@@ -61,6 +62,3 @@ switch (op)
 
     return 0;
 }
-
-
-
