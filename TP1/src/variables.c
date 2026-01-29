6647a514bd1f1de3d4de6327b25744698094505c
SerkanHub <ozerserkan05@gmail.com>
2026-01-29 10:54:52 +0100

    Update variables.c

--- TP1/src/variables.c
+++ TP1/src/variables.c
@@ -1,12 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main ()
+int main(void)
 {
-  signed char Ca = -128;
-  unsigned char Cb = 255;
-  printf("Valeur de Ca = %d\n", Ca);
-  printf("Valeur de Cb = %d\n", Cb);
+    /* i. char */
+    signed char c1 = -65;
+    unsigned char c2 = 200;
 
-  return 0;
+    /* ii. short */
+    signed short s1 = -32000;
+    unsigned short s2 = 65000;
+
+    /* iii. int */
+    signed int i1 = -100000;
+    unsigned int i2 = 300000;
+
+    /* iv. long int */
+    signed long int l1 = -1000000;
+    unsigned long int l2 = 4000000;
+
+    /* v. long long int */
+    signed long long int ll1 = -9000000000;
+    unsigned long long int ll2 = 18000000000;
+
+    /* vi. float */
+    float f = 3.14;
+
+    /* vii. double */
+    double d = 2.718281828;
+
+    /* viii. long double */
+    long double ld = 1.6180339887;
+
+    /* Affichage des valeurs */
+    printf("signed char : %d\n", c1);
+    printf("unsigned char : %u\n\n", c2);
+
+    printf("signed short : %d\n", s1);
+    printf("unsigned short : %u\n\n", s2);
+
+    printf("signed int : %d\n", i1);
+    printf("unsigned int : %u\n\n", i2);
+
+    printf("signed long int : %ld\n", l1);
+    printf("unsigned long int : %lu\n\n", l2);
+
+    printf("signed long long int : %lld\n", ll1);
+    printf("unsigned long long int : %llu\n\n", ll2);
+
+    printf("float : %f\n", f);
+    printf("double : %lf\n", d);
+    printf("long double : %Lf\n", ld);
+
+    return 0;
 }
