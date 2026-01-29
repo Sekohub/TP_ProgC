5d4fec142b147526c205d0b551cef3ff6f4f3a7e
SerkanHub <ozerserkan05@gmail.com>
2026-01-29 12:23:39 +0100

    Update puissance.c

---
+++ TP2/src/puissance.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int a = 6;
+  int b = 5;
+  int i;
+  float puiss = 1; 
+
+  for (i = 1, i <= b, i++)
+    {
+      puiss = puiss * a
+    }
+
+  printf("%d à la puissance %d = %d\n", a, b, puiss);
+
+  return 0;
+}
