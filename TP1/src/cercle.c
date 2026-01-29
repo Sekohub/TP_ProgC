d1f8c07c2e83ec40af702494ec4cc338a5ce82e0
SerkanHub <ozerserkan05@gmail.com>
2026-01-29 10:08:38 +0100

    Update cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -4,8 +4,9 @@
 int main() {
 double pi = 3.1416; // Valeur de "pi"
 double r = 6;  // Variable "rayon" 
-    printf("L'air du cercle = %f", pi*(r*r)); // %f affiche "double"
-   printf("Le périmètre du cercle = %f", pi*(r+r));
+    printf("L'air du cercle = %f\n", pi*(r*r)); // %f affiche "double"
+   printf("Le périmètre du cercle = %f\n", pi*(r+r));
 }
+
 
 
