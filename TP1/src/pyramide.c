c1679cc96abd64fbd58bb222d3f04c4b92c18cc7
SerkanHub <ozerserkan05@gmail.com>
2026-01-29 12:08:31 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 5;   /* hauteur de la pyramide */
+    int i, j;
+
+    /* Génération de la pyramide */
+    for (i = 1; i <= n; i++)
+    {
+        /* Affichage des espaces pour centrer */
+        for (j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+
+        /* Affichage des nombres croissants */
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+
+        /* Affichage des nombres décroissants */
+        for (j = i - 1; j >= 1; j--)
+        {
+            printf("%d", j);
+        }
+
+        /* Passage à la ligne suivante */
+        printf("\n");
+    }
+
+    /* Fin du programme */
+    printf("\nGeneration de la pyramide terminee.\n");
+
+    return 0;
+}
 
