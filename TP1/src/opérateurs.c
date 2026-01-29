035777b243b44711d6706488b6a5f619c6c7277c
SerkanHub <ozerserkan05@gmail.com>
2026-01-29 11:24:53 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+
+int main(void)
 {
-  int a = 16;
-  int b = 3;
-  float addition = a + b;
-  printf("%d + %d = %f", a, b, addition)
+    int a = 16;
+    int b = 3;
 
-  return 0, 
+    /* Opérations */
+    printf("Addition : %d\n", a + b);
+    printf("Soustraction : %d\n", a - b);
+    printf("Multiplication : %d\n", a * b);
+    printf("Division : %d\n", a / b);
+    printf("Modulo : %d\n\n", a % b);
 
+    /* Opérateurs de comparaison (résultat booléen : 0 ou 1) */
+    printf("a égale à b ? : %d\n", a == b);
+    printf("a plus petit que b ? : %d\n", a > b);
+
+    return 0;
 }
+
 
 
