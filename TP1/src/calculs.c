802102a0d61b7e31315ee049ff6990d39023cfd1
SerkanHub <ozerserkan05@gmail.com>
2026-01-29 11:58:20 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int num1;
+  int num2;
+  char op;
+
+  printf("Entrez le premier nombre entier : ");
+    scanf("%d", &num1);
+
+    printf("Entrez le second nombre entier : ");
+    scanf("%d", &num2);
+
+    printf("Entrez l'operateur (+, -, *, /, %%, &, |, ~) : ");
+    scanf(" %c", &op); 
+
+switch (op)
+    {
+        case '+':
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+
+        case '-':
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+
+        case '*':
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0)
+                printf("%d / %d = %d\n", num1, num2, num1 / num2);
+            else
+                printf("Erreur : division par zero\n");
+            break;
+
+        case '%':
+            if (num2 != 0)
+                printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+            else
+                printf("Erreur : modulo par zero\n");
+            break;
+
+        case '&':
+            printf("%d & %d = %d\n", num1, num2, num1 & num2);
+            break;
+
+        case '|':
+            printf("%d | %d = %d\n", num1, num2, num1 | num2);
+            break;
+
+        case '~':
+            printf("~%d = %d\n", num1, ~num1);
+            break;
+
+        default:
+            printf("Operateur non reconnu\n");
+    }
+
+    return 0;
+}
+
 
