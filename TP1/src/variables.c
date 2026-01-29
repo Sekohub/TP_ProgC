2678b8e31d6825ee4b9a287b6b36f51a70cec80c
SerkanHub <ozerserkan05@gmail.com>
2026-01-29 10:44:44 +0100

    Update variables.c

---
+++ TP1/src/variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+  signed char Ca = -128;
+  unsigned char Cb = 255;
+  printf("Valeur de Ca = %d\n", Ca);
+  printf("Valeur de Cb = %d\n", Cb);
+
+  return 0;
+}
