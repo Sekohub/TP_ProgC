c06d7caaea3f30688621972402200e75c3848df7
SerkanHub <ozerserkan05@gmail.com>
2026-01-29 12:37:34 +0100

    Update bits.c

--- TP2/src/bits.c
+++ TP2/src/bits.c
@@ -2,18 +2,12 @@
 
 int main(void)
 {
-    int d = 0b10101101; /* valeur de test 173 bits */
-    int bit4_Gauche;
-    int bit20_Gauche;
+    int d = 173;   
 
-    /* Extraction du 4e bit de gauche (position 28) */
-    bit4_Gauche = (d >> 28) & 1;
+    int bit4_gauche  = (d >> 28) & 1;
+    int bit20_gauche = (d >> 12) & 1;
 
-    /* Extraction du 20e bit de gauche (position 12) */
-    bit20_Gauche = (d >> 12) & 1;
-
-    /* Vérification */
-    if (bit4_Gauche == 1 && bit20_Gauche == 1)
+    if (bit4_gauche == 1 && bit20_gauche == 1)
         printf("1\n");
     else
         printf("0\n");
